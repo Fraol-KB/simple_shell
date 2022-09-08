0430bb3ab56c0509de7b04507da8355a57c2b2ba
gechgb <gechgberhe@gmail.com>
2022-09-08 19:25:39 +0300

    prototypes get_line bring_line functions added

---
+++ main.h
@@ -0,0 +1,44 @@
+#ifndef _main_
+#define _main_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 2
+#define TOK_DELIM " \t\r\n\a"
+
+/* aux_str functions */
+char *_strcat(char *dest, const char *src);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+
+/* aux_mem.c */
+void _memcpy(void *newptr, const void *ptr, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* aux_str2.c */
+char *_strdup(const char *s);
+int _strlen(const char *s);
+int cmp_chars(char str[], const char *delim);
+char *_strtok(char str[], const char *delim);
+
+/* shell_loop.c */
+void shell_loop(void);
+
+/* read_line.c */
+char *read_line(void);
+
+/* split_line.c */
+char **split_line(char *input);
+
+#endif
